0babb2a7158cf84eef407cbe6a05a52cd1080e50
EduardoRG <eduruizgonza@gmail.com>
2017-03-14 13:57:11 +0100

    Coverage reports with codecov

--- karma.conf.js
+++ karma.conf.js
@@ -35,11 +35,13 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['coverage', 'coveralls'],
+    reporters: ['progress', 'coverage'],
 
     coverageReporter: {
-      type: 'lcov',
-      dir: 'coverage/',
+      reporters: [
+        {type: 'lcov'},
+        {dir: 'coverage/'},
+      ]
     },
 
     // web server port
